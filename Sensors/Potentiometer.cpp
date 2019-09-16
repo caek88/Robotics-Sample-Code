9fc929e2c8fdc5495c5190fc500a0db3e503026d
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-16 12:51:30 -0400

    Update Potentiometer.cpp

--- Sensors/Potentiometer.cpp
+++ Sensors/Potentiometer.cpp
@@ -1,18 +1,18 @@
+#include "robot-config.h"
 //This program is for using the potentiometer
 //A potentiometer is used to measure how far an axle has turned and has a limited range of turning (It will break if you turn it too far)
 //This is an analog sensor
 using namespace vex;
-brain Brain;
-pot potFoo = pot(Brain.ThreeWirePort.A);//Create the potentiometer object on three wire port A
+//Create the potentiometer object on three wire port A called PotFoo
 int main() {
     while (true){
         Brain.Screen.clearScreen();
         Brain.Screen.setCursor(1,1);
-        Brain.Screen.print("%d\n", potFoo.value(percentUnits::pct));//Measure as a percentage
+        Brain.Screen.print("%d\n", PotFoo.value(percentUnits::pct));//Measure as a percentage
         Brain.Screen.newLine();
-        Brain.Screen.print("%d\n", potFoo.value(rotationUnits::deg));//Measure as an angle * currently doesn't work
+        Brain.Screen.print("%d\n", PotFoo.value(rotationUnits::deg));//Measure as an angle * currently doesn't work
         Brain.Screen.newLine();
-        Brain.Screen.print("%d", potFoo.value(analogUnits::mV));//Measure in millivolts
+        Brain.Screen.print("%d", PotFoo.value(analogUnits::mV));//Measure in millivolts
         task::sleep(10);
     }
 }
