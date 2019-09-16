ca94dbef4bb4dcdcb84d0a1b230c6b554bd71c5d
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-16 12:18:47 -0400

    Update Moving Straight Rotations.cpp

--- Motors/Moving Straight Rotations.cpp
+++ Motors/Moving Straight Rotations.cpp
@@ -1,20 +1,16 @@
+#include "robot-config.h"
 //This program is for making the robot move straight with a two motor drive
 using namespace vex;//Reference the vex class
-
-//Create the motor objects for the left and right motors
-motor mtrLeft = motor(PORT1, false); //Set the left motor on port 1 with its direction not reversed
-motor mtrRight = motor(PORT2, true); //Set the right motor on port 2 with its direction reversed
-
 int main(){
     //Make the robot drive straight for 2 rotations
-    mtrLeft.resetRotation();//Reset the motor encoder rotations
-    mtrRight.resetRotation();
-    mtrLeft.spin(directionType::fwd, 100, percentUnits::pct);
-    mtrRight.spin(directionType::fwd, 100, percentUnits::pct);//Set each motor to 100% power forwards
+    MtrLeft.resetRotation();//Reset the motor encoder rotations
+    MtrRight.resetRotation();
+    MtrLeft.spin(directionType::fwd, 100, percentUnits::pct);
+    MtrRight.spin(directionType::fwd, 100, percentUnits::pct);//Set each motor to 100% power forwards
     
-    while (mtrLeft.rotation(rotationUnits::rot) < 2){//This is the only line of code different from the degree program
+    while (MtrLeft.rotation(rotationUnits::rot) < 2){//This is the only line of code different from the degree program
         task::sleep(25);//Check every 25 milliseconds
     }//wait until the left motor has gone 2 rotations
-    mtrLeft.stop();
-    mtrRight.stop();//Stop both motors (They are still spinning)
+    MtrLeft.stop();
+    MtrRight.stop();//Stop both motors (They are still spinning)
 }
