237668b17968965209bc3faf4c0fb02677b5ce40
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-16 12:27:14 -0400

    Update Point Turns.cpp

--- Motors/Point Turns.cpp
+++ Motors/Point Turns.cpp
@@ -6,8 +6,8 @@ int main(){
     //To make the robot, you have to set the two motors to different speeds
     //In a point turn, one side of the chassis moves forward while the other moves backward
     //Right Point Turn:
-    MtrLeft.spin(directionType::fwd, 100, percentUnits::pct);//Set the left motor to 100% power forwards
-    MtrRight.spin(directionType::fwd, -100, percentUnits::pct);//Set the right motor to -100% power
+    MtrLeft.spin(directionType::fwd, 100, velocityUnits::pct);//Set the left motor to 100% power forwards
+    MtrRight.spin(directionType::fwd, -100, velocityUnits::pct);//Set the right motor to -100% power
     //Even though the directionType is forward, the negative makes the motor spin backwards
     task::sleep(1000);//Pause for 1 second (1000 milliseconds)
     MtrLeft.stop();
