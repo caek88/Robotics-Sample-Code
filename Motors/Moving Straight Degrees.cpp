3302397e5a86e0a32ceb6aa3e69e94577399a2c6
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-16 12:28:46 -0400

    Update Moving Straight Degrees.cpp

--- Motors/Moving Straight Degrees.cpp
+++ Motors/Moving Straight Degrees.cpp
@@ -6,8 +6,8 @@ int main(){
     //Make the robot drive straight for 720 degrees (2 rotations)
     MtrLeft.resetRotation();//Reset the motor encoder rotations
     MtrRight.resetRotation();
-    MtrLeft.spin(directionType::fwd, 100, percentUnits::pct);
-    MtrRight.spin(directionType::fwd, 100, percentUnits::pct);//Set each motor to 100% power forwards
+    MtrLeft.spin(directionType::fwd, 100, velocityUnits::pct);
+    MtrRight.spin(directionType::fwd, 100, velocityUnits::pct);//Set each motor to 100% power forwards
     
     while (MtrLeft.rotation(rotationUnits::deg) < 720){
         task::sleep(25);//Check every 25 milliseconds
