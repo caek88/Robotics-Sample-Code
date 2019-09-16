384eac49328230ef228dcae4d3ecd354264f0bf3
caek88 <35225849+caek88@users.noreply.github.com>
2019-09-16 12:23:29 -0400

    Update Moving Straight Alternative.cpp

--- Motors/Moving Straight Alternative.cpp
+++ Motors/Moving Straight Alternative.cpp
@@ -1,20 +1,17 @@
+#include "robot-config.h"
 //This program uses an alternative function to make the motors move a certain more distance
 //This function is more accurate than the other methods
 using namespace vex;//Reference the vex class
 
-//Create the motor objects for the left and right motors
-motor mtrLeft = motor(PORT1, false); //Set the left motor on port 1 with its direction not reversed
-motor mtrRight = motor(PORT2, true); //Set the right motor on port 2 with its direction reversed
-
 int main(){
     //Make the robot drive straight for 720 degrees (2 rotations)
-    mtrLeft.resetRotation();//Reset the motor encoder rotations
-    mtrRight.resetRotation();
-    mtrLeft.startRotateTo(720, rotationUnits::deg, 100, percentUnits::pct); //Make the left motor move to 720 degrees
-    mtrRight.startRotateTo(720, rotationUnits::deg, 100, percentUnits::pct); //Make the right motor move to 720 degrees
+    MtrLeft.resetRotation();//Reset the motor encoder rotations
+    MtrRight.resetRotation();
+    MtrLeft.startRotateTo(720, rotationUnits::deg, 100, velocityUnits::pct); //Make the left motor move to 720 degrees
+    MtrRight.startRotateTo(720, rotationUnits::deg, 100, velocityUnits::pct); //Make the right motor move to 720 degrees
     //The startRotateTo command will move to 720 degrees and then holds position
     //The startRotateTo command can be used to make multiple motors spin to different distances (The left motor goes to one position while the right motor goes to the other)
-    while (!mtrLeft.spinning() && !mtrRight.spinning()){
+    while (!MtrLeft.isSpinning() && !MtrRight.isSpinning()){
         task::sleep(25);//Check every 25 milliseconds
     }//wait until both motors have stopped
 }
